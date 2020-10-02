4c7173b48c7ad9fd7a9d0634c1d514766d31fa25
Martin Moench <martim09@phs.scot>
2020-10-02 15:37:58 +0100

    First commit

---
+++ Old-Faithful/server.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(shinymanager)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+  
+  res_auth <- secure_server(
+    check_credentials = check_credentials(credentials)
+  )
+  
+  output$auth_output <- renderPrint({
+    reactiveValuesToList(res_auth)
+  })
+   
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+})
